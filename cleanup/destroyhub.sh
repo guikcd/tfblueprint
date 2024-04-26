97b13296d9034a0438e3a006b017c050a0f25d40
EC2 Default User <ec2-user@ip-172-31-43-97.us-west-2.compute.internal>
2024-04-26 11:09:16 +0000

    sdf

---
+++ cleanup/destroyhub.sh
@@ -0,0 +1,4 @@
+cd ~/environment/hub
+terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
+terraform destroy -target="module.eks_blueprints_addons" -auto-approve
+terraform destroy -target="module.eks" -auto-approve
